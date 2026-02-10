dfc95b1ab493aaf0fdc2128b8e447c7051ac4d2a
Nikodem Wspanialy <nikodemwspanialy@gmail.com>
2026-02-10 01:13:16 +0100

    add route for requests

--- backend/CarMarketplace/CarMarketplace.API/Controllers/CarController.cs
+++ backend/CarMarketplace/CarMarketplace.API/Controllers/CarController.cs
@@ -9,17 +9,17 @@ using Microsoft.AspNetCore.Mvc;
 namespace CarMarketplace.API.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/car")]
 public class CarController(IMediator mediator) : ControllerBase
 {
-    [HttpPost]
+    [HttpPost("create")]
     public async Task<IActionResult> Create([FromBody] CreateCarRequest command)
     {
         var id = await mediator.Send(command);
         return CreatedAtAction(nameof(GetById), new { id }, null);
     }
 
-    [HttpPut("{id:guid}")]
+    [HttpPut("update-details/{id:guid}")]
     public async Task<IActionResult> Update(Guid id, [FromBody] UpdateCarRequest command)
     {
         if (id != command.Id) return BadRequest("Id mismatch");
@@ -27,21 +27,21 @@ public class CarController(IMediator mediator) : ControllerBase
         return NoContent();
     }
 
-    [HttpDelete("{id:guid}")]
+    [HttpDelete("delete/{id:guid}")]
     public async Task<IActionResult> Delete(Guid id)
     {
         await mediator.Send(new DeleteCarRequest(id));
         return NoContent();
     }
 
-    [HttpGet("{id:guid}")]
+    [HttpGet("get-details/{id:guid}")]
     public async Task<IActionResult> GetById(Guid id)
     {
         var car = await mediator.Send(new GetCarRequest(id));
         return Ok(car);
     }
 
-    [HttpGet]
+    [HttpGet("get-details-list")]
     public async Task<IActionResult> GetPaged([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
     {
         var result = await mediator.Send(new GetCarsRequest(pageNumber, pageSize));
