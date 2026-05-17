d7930fd15b1d498fb022e1f8563ae270dbf0c6ee
nikodem.wspanialy <nikodem.wspanialy@shijigroup.com>
2026-05-17 19:31:02 +0200

    use new response in car flow

--- backend/CarMarketplace/CarMarketplace.Application/Cars/DTOs/CarListResponse.cs
+++
@@ -1,3 +0,0 @@
-namespace CarMarketplace.Application.Cars.DTOs;
-
-public record CarListResponse(IReadOnlyList<CarResponse> Items, int TotalCount);
--- backend/CarMarketplace/CarMarketplace.Application/Cars/Queries/GetCars/GetCarsHandler.cs
+++ backend/CarMarketplace/CarMarketplace.Application/Cars/Queries/GetCars/GetCarsHandler.cs
@@ -1,18 +1,19 @@
 using CarMarketplace.Application.Cars.DTOs;
 using CarMarketplace.Application.Cars.Repositories;
+using CarMarketplace.Application.Common.DTOs;
 using MediatR;
 
 namespace CarMarketplace.Application.Cars.Queries.GetCars;
 
 internal class GetCarsHandler(
-    ICarRepository carRepository) 
-    : IRequestHandler<GetCarsRequest, CarListResponse>
+    ICarRepository carRepository)
+    : IRequestHandler<GetCarsRequest, ListResponse<CarResponse>>
 {
-    public async Task<CarListResponse> Handle(GetCarsRequest request, CancellationToken token)
+    public async Task<ListResponse<CarResponse>> Handle(GetCarsRequest request, CancellationToken token)
     {
         var result = await carRepository.GetPagedAsync(request.PageNumber, request.PageSize, token);
         var items = result.Cars.Select(CarResponse.FromEntity).ToList();
 
-        return new CarListResponse(items, result.TotalCount);
+        return new ListResponse<CarResponse>(items, result.TotalCount, request.PageNumber, request.PageSize);
     }
 }
