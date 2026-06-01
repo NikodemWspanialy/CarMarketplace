321d6b688d2f2e2b6104ae706d26bb33f746b18d
nikodem.wspanialy <nikodem.wspanialy@shijigroup.com>
2026-06-02 00:03:08 +0200

    fix 1 and 2

--- backend/CarMarketplace/CarMarketplace.Application/Authorization/Queries/LoginUser/LoginUserHandler.cs
+++ backend/CarMarketplace/CarMarketplace.Application/Authorization/Queries/LoginUser/LoginUserHandler.cs
@@ -20,7 +20,7 @@ internal class LoginUserHandler(
             throw new InvalidCredentials();
         }
 
-        if (!passwordHasher.VerifyHashedPassword(request.Password, user.PasswordHash))
+        if (!passwordHasher.VerifyHashedPassword(user.PasswordHash, request.Password))
         {
             throw new InvalidCredentials();
         }
