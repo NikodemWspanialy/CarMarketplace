4c10e1c158705bb0225fdc68e1d32b909855b755
nikodem.wspanialy <nikodem.wspanialy@shijigroup.com>
2026-06-06 16:54:48 +0200

    validation to change email by user

--- backend/CarMarketplace/CarMarketplace.Application/Extensions/DependencyInjection.cs
+++ backend/CarMarketplace/CarMarketplace.Application/Extensions/DependencyInjection.cs
@@ -8,6 +8,7 @@ using CarMarketplace.Application.Common.Behaviors;
 using CarMarketplace.Application.Users.Factories;
 using CarMarketplace.Application.Users.Helpers;
 using CarMarketplace.Application.Users.Searchers;
+using CarMarketplace.Application.Users.Validators;
 using FluentValidation;
 using MediatR;
 
@@ -29,6 +30,7 @@ public static class DependencyInjection
 
         // Validators
         services.AddScoped<IRegisterUserValidator, RegisterUserValidator>();
+        services.AddScoped<IChangeEmailValidator, ChangeEmailValidator>();
 
         // Factories
         services.AddScoped<IUserFactory, UserFactory>();
